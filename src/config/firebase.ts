0be4fcad20550ce90400838d9feade9605f1cb83
Yakymiv <yakymiv.0706@gmail.com>
2023-05-17 20:28:01 +0300

    text with env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -12,7 +12,8 @@ const firebaseConfig = {
 console.log(process.env);
 console.log(
   process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
+  process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
 );
 
 let firebase =
